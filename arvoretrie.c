aa483b5d48e3c7d229d91546e34da2d92910040f
Pedro <pedromarcelino2006@hotmail.com>
2014-10-07 21:17:29 -0300

    Inserindo token na arvore

--- arvoretrie.c
+++ arvoretrie.c
@@ -13,26 +13,48 @@
 struct nodo{
 	struct nodo *v[k];
 	char flag;	
-}
+};
 
 struct nodo *root = NULL;
 
+
 //funcao para inserir uma nova palavra na arvore
-void inserir(struct node **nodo, char *palavra);
+void inserir(struct nodo **node, char *palavra);
 //funcao para realizar uma busca de uma palavra na arvore
-int buscar(struct node *nodo, char *palavra);
+int buscar(struct nodo *node, char *palavra);
 
 
 int main(int argc, char *argv[]){
 
 	FILE *arquivo1, *arquivo2;
-	char caracter;
+	char caracter, carac_dicio;
+	char token[100];
+	int i = 0;
 
 	arquivo1 = fopen(argv[1], "r");
 	//arquivo2 = fopen(argv[2], "a");
 
-	while((caracter = fgetc(arquivo1)) != EOF){
+	//lendo caracter por caracter para inserir no dicionario
+	while((carac_dicio = fgetc(arquivo1)) != EOF){
 
+		if(carac_dicio != '\n'){
+			token[i] = carac_dicio;
+			i++;
+		}
+		else{
+			token[i] = '\0';
+			i = 0;
+			inserir(&())
+		}
+
+	}
+
+	//lendo todos os caracteres do arquivo2
+	while((caracter = fgetc(arquivo2)) != EOF){
+
+		if((caracter != ' ') && (caracter != '-')){
+
+		}
 		//COLOCAR AQUI OS METODOS DE ARMAZENAR A STRING QUANDO ENCONTRAR UM SEPARADOR
 		//VERIFICAR SE A VARIAVEL ESTA CONTIDA NO DICIONARIO
 		//SE NAO ESTIVER CONTIDA, INSERIR NA VARIAVEL
@@ -45,34 +67,36 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-void inserir(struct node **nodo, char *palavra){
+void inserir(struct nodo **node, char *palavra){
+
 	int i;
 
-	if(*nodo == NULL){
-		*nodo = malloc(sizeof(struct nodo));
+	if(*node == NULL){
+		*node = malloc(sizeof(struct nodo));
 		for(i = 0; i < k; i++)
-			(*nodo)->v[i] = NULL;
-		if(palavra[0] != '\0'){
-			inserir(&(*nodo)->v[palavra[0] - 'a'], palavra + 1);
-			(*nodo)->flag = 0;
+			(*node)->v[i] = NULL;
+		if(*palavra != '\0'){
+			inserir(&(*node)->v[*palavra - 'a'], palavra + 1);
+			(*node)->flag = 0;
 		}
 		else
-			(*nodo)->flag = 1;
+			(*node)->flag = 1;
 	}
-	else
-		if(palavra[0] == '\0'){
-			inserir(&(*nodo)->v[palavra[0] - 'a'], palavra + 1);
+	else{
+		if(*palavra == '\0'){
+			inserir(&(*node)->v[*palavra - 'a'], palavra + 1);
 		}
 		else
-			(*nodo)->flag = 1;
+			(*node)->flag = 1;
+	}
 }
 
-int buscar(struct node *nodo, char *palavra){
-	if(nodo == NULL)
+int buscar(struct nodo *node, char *palavra){
+	if(node == NULL)
 		return 0;
 	else
 		if(palavra[0] == '\0')
-			return (nodo->flag ? 1:0);
+			return (node->flag ? 1:0);
 		else
-			return buscar(nodo->v[palavra[0] - 'a'], palavra + 1);
+			return buscar(node->v[palavra[0] - 'a'], palavra + 1);
 }
